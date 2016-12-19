47f544bfb8c9dbf7676c3e67bbc87479535295ee
bpsou <bpsou@asusrog>
2016-12-19 21:41:50 +0000

    Added signal schema to the project

--- ConsoleApplication5/Program.cs
+++ ConsoleApplication5/Program.cs
@@ -4,6 +4,7 @@ using System.Net;
 using System.Text;
 using System.Xml;
 using System.Xml.Schema;
+using uPLibrary.Networking.M2Mqtt;
 using uPLibrary.Networking.M2Mqtt.Messages;
 
 namespace ConsoleApplication5
